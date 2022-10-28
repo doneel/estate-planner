0c01a18186a59a711b327cbbfdea88f4b8f14261
Daniel O'Neel <danieloneel@gmail.com>
2022-10-27 20:39:42 -0700

    remove e2e tests that break now that front page is not available

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Estate Planner",
   viewport: "width=device-width,initial-scale=1",
 });
 
