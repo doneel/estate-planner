e563c928a7cbffd24c1474c1eda375c3f55d4907
Daniel O'Neel <danieloneel@gmail.com>
2022-11-10 20:26:38 -0800

    footer url

--- app/components/footer/GlobalFooter.tsx
+++ app/components/footer/GlobalFooter.tsx
@@ -4,7 +4,7 @@ export default function GlobalFooter() {
       <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
       <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
         © 2022{" "}
-        <a href={process.env.HOST} className="hover:underline">
+        <a href="https://flowbite.com/" className="hover:underline">
           Mandos Estates™
         </a>
         . All Rights Reserved.
