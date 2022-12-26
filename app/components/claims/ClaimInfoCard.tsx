21f77037561d8dcb38e537f282984d62e651872e
Daniel O'Neel <danieloneel@gmail.com>
2022-12-26 17:52:23 -0300

    activity mock page

--- app/components/claims/ClaimInfoCard.tsx
+++ app/components/claims/ClaimInfoCard.tsx
@@ -23,25 +23,36 @@ export default function ClaimInfoCard({ claim, size = "sm" }: Props) {
       </div>
       <div className="flex flex-col md:flex-row md:justify-between">
         <div className="flex flex-col items-start md:items-end">
-          <p className="text-sm font-light">
+          <p className={`${size === "sm" ? "text-xs" : "text-sm"} font-light`}>
             File <strong className="font-semibold">{claim.file_number}</strong>
           </p>
-          <p className="text-sm font-light">
+          <p className={`${size === "sm" ? "text-xs" : "text-sm"} font-light`}>
             Claim{" "}
             <strong className="font-semibold">{claim.claim_number}</strong>
           </p>
         </div>
         <div className="flex flex-col items-start md:items-end">
-          <div className="flex">
-            <FaRegCalendar className="w-3 text-slate-500" />
-            <p className="ml-2 text-sm font-light">
+          <div className="flex items-center">
+            {/* <FaRegCalendar className="w-3 text-slate-500" /> */}
+            <p
+              className={`${
+                size === "sm" ? "text-xs" : "text-sm"
+              } ml-2 font-light`}
+            >
               Due <strong className="font-semibold">12/12/22</strong>
             </p>
           </div>
-          <div className="flex">
-            <FaRegUser className="w-3 text-slate-500" />
+          <div className="flex items-center">
+            {/* <FaRegUser className="w-3 text-slate-500" /> */}
             {/* TODO: Adjusters should be clickable. Just filter for their claims? */}
-            <p className="ml-2 text-sm">Me, Mike Jones</p>
+
+            <p
+              className={`${
+                size === "sm" ? "text-xs" : "text-sm"
+              } ml-1 font-light`}
+            >
+              Me, Mike Jones
+            </p>
           </div>
         </div>
       </div>
---
+++ app/routes/adjusting/claims/$claimId/activity.tsx
@@ -0,0 +1,72 @@
+import { NavLink } from "@remix-run/react";
+import { FaMailBulk, FaPrint, FaRegCalendar } from "react-icons/fa";
+import EmailActivity from "~/components/activities/EmailActivity";
+
+export default function Activity() {
+  return (
+    <div className="p-16">
+      <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        <li className="relative mb-10 ml-6">
+          <span className="absolute -left-9 top-4 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+            <FaPrint />
+          </span>
+          <div className="items-center justify-between rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-600 dark:bg-gray-700 sm:flex">
+            <div className="sm:order-last">
+              <span className="mr-2 rounded bg-gray-100 px-2.5 py-0.5 text-xs font-normal text-gray-800 dark:bg-gray-600 dark:text-gray-300">
+                Pending
+              </span>
+              <time className="mb-1 text-xs font-normal text-gray-400  sm:mb-0">
+                {new Date(2022, 11, 12, 12, 37).toLocaleDateString()}{" "}
+                {new Date(2022, 11, 12, 12, 37).toLocaleTimeString()}
+              </time>
+            </div>
+            <div className="text-sm font-normal text-gray-500 dark:text-gray-300">
+              Claim received from United Car Insurance National.
+            </div>
+          </div>
+        </li>
+        <EmailActivity
+          from={"Mark Jackson"}
+          emailPreviewText={
+            "Hi John, this is Mark reaching out from Example Adjusters, Inc. about your recent auto insurance claim\n. I'd love to get someone out to take a look this week. Will you be around Monday or Tuesday?"
+          }
+          date={new Date(2022, 11, 12, 15, 22, 0)}
+          status={"active"}
+        />
+        <EmailActivity
+          from={"John Ziegler"}
+          emailPreviewText={
+            "Thanks Mark. I'm available on Tuesday morning before 11:15. Does that work for you?"
+          }
+          date={new Date(2022, 11, 13, 8, 7, 0)}
+          status={"active"}
+        />
+        <li className="relative mb-10 ml-6">
+          <span className="absolute -left-9 top-4 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+            <FaRegCalendar />
+          </span>
+          <div className="items-center justify-between rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-600 dark:bg-gray-700 sm:flex">
+            <div className="sm:order-last">
+              <span className="mr-2 rounded bg-green-100 px-2.5 py-0.5 text-xs font-normal text-gray-800 dark:bg-gray-600 dark:text-gray-300">
+                Active
+              </span>
+              <time className="mb-1 text-xs font-normal text-gray-400  sm:mb-0">
+                {new Date(2022, 11, 12, 12, 37, 13, 12).toLocaleDateString()}{" "}
+                {new Date(2022, 11, 12, 12, 37, 13, 12).toLocaleTimeString()}
+              </time>
+            </div>
+            <div className="text-sm font-normal text-gray-500 dark:text-gray-300">
+              <NavLink
+                to="#"
+                className="font-semibold text-blue-600 hover:underline dark:text-blue-500"
+              >
+                Mark Jackson
+              </NavLink>{" "}
+              scheduled an inspection for Tuesday 11/15 at 9:30am
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
+  );
+}
