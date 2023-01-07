f3cd5bb337195ff606c7e3443c21fce260d69895
Daniel O'Neel <danieloneel@gmail.com>
2023-01-06 22:29:39 -0800

    task notification

--- app/routes/adjusting/notifications.tsx
+++ app/routes/adjusting/notifications.tsx
@@ -1,12 +1,26 @@
-import { useLoaderData } from "@remix-run/react";
+import type { Claim } from "@prisma/client";
+import { NavLink, useLoaderData } from "@remix-run/react";
+import type { SerializeFrom } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
+import { stringify } from "querystring";
+import { useEffect } from "react";
+import TaskNotification from "~/components/notifications/TaskNotification";
 
-export interface TaskNotification {
+export interface TaskNotificationData {
   id: string;
   type: "task";
   title: string;
   preview?: string;
   assignedBy: string;
+  createdAt: Date;
+  claimId: Claim["id"];
+  claimNumber: Claim["claim_number"];
+}
+
+function isTask(
+  notification: SerializeFrom<Notification>
+): notification is SerializeFrom<TaskNotificationData> {
+  return notification.type === "task";
 }
 
 export interface MessageNotification {
@@ -15,9 +29,10 @@ export interface MessageNotification {
   title: string;
   preview?: string;
   from: string;
+  createdAt: Date;
 }
 
-export type Notification = TaskNotification | MessageNotification;
+export type Notification = TaskNotificationData | MessageNotification;
 
 export async function loader() {
   return json({
@@ -27,6 +42,9 @@ export async function loader() {
         type: "task",
         title: "Reschedule Michael Trotter (adjuster)",
         assignedBy: "Amy Alvarez",
+        createdAt: new Date(2022, 12, 19, 9, 34, 3),
+        claimId: "clbziq25c0007c9kq7doye2sk",
+        claimNumber: "PC23409A3",
       },
       {
         id: "2",
@@ -34,6 +52,7 @@ export async function loader() {
         title: "Re: Your claim with United Auto Insurance",
         preview: "Yeah hi that all looks correct, when can you guys come out?",
         from: "Mike Jones",
+        createdAt: new Date(2022, 12, 19, 12, 9, 3),
       },
     ],
   });
@@ -41,5 +60,27 @@ export async function loader() {
 
 export default function Notifications() {
   const { notifications } = useLoaderData<typeof loader>();
-  return <div className="flex h-full"></div>;
+
+  useEffect(() => {
+    async function importFlowbite() {
+      // @ts-ignore
+      await import("../../../node_modules/flowbite/dist/flowbite");
+    }
+    importFlowbite();
+  });
+  return (
+    <div className="mx-auto flex h-full w-full items-start justify-center overflow-y-scroll py-4">
+      <div className="w-full md:w-1/2 md:max-w-[50%]">
+        {notifications.map((notification) => {
+          return isTask(notification) ? (
+            <TaskNotification
+              notification={{ ...notification, type: "task" }}
+            />
+          ) : (
+            <></>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
